00921f5398ebacfcf8b099f61e3c2d15436ac658
Zhaozhou Guo <yidanguo1102@gmail.com>
2019-03-14 00:50:03 +0800

    对近义词结果进行判断是否需要添加到数据库other_names列中，暂只判断，而不存储数据

--- othernames_data_push.php
+++ othernames_data_push.php
@@ -1,11 +1,7 @@
 <?php 
 	require('req_globals.php');
-	// mysql_query('set names utf8')//面向过程， 编程方式；
-	// mysqli_set_charset ($link,'utf8')//面向对象 ，编程方式；
-	// mysqli::set_charset('utf8')//面向对象，编程方式；
+	require_once 'qcloudapi-sdk-php-master/src/QcloudApi/QcloudApi.php';//腾讯文智sdk同义词api
 	mysqli_query($con, 'set names utf8');
-	$mat_boss = mysqli_query($con,"SELECT * FROM mats_data WHERE id = 1 ");
-	$mat_first = mysqli_fetch_assoc($mat_boss);
 	// if (!$mat_first) {
 	// printf("Error: %s\n", mysqli_error($con));
 	// exit();
@@ -23,37 +19,83 @@
 	</head>
 	<body>
 		<?php 
+		$mat_list = mysqli_query($con,"SELECT * FROM mats_data");
+		$mat_list_worker = mysqli_fetch_all($mat_list);
+		$mat_name_list = array();
+		foreach ($mat_list_worker as $value) {
+			array_push($mat_name_list, $value[1]);
+		}
 
-		echo $mat_first["name"];
+
+		$index = 1;
+		$max_index = 1;
+		while($index <= $max_index){
+			$mat_boss = mysqli_query($con,"SELECT * FROM mats_data WHERE id = $index ");
+			$mat = mysqli_fetch_assoc($mat_boss);
+			$mat_other_names = explode(";",$mat["other_names"]);
+			// print_r($mat_other_names);
+
+			// $Synonyms = tongyici_wenzhi($mat["name"]);
+			$Synonyms = ["肥肠","猪肉","大肉","肉末"];
+			if($Synonyms){
+				// echo $index . " " . $mat["name"] . ":";
+				// print_r($Synonyms);
+				foreach($Synonyms as $Synonym){
+					if (!in_array($Synonym, $mat_name_list) && !in_array($Synonym, $mat_other_names)){//如果没有在现有数据库的"name"列中
+						echo $Synonym . "已添加到" . $mat["name"] . "别名。";
+					}else{
+						echo $Synonym . "已经存在。";
+					}
+				}
+			}
+			
+
+			$index += 1;
+		}
+		
+
+		function update_othernames($index, $othernames){
+			mysql_query("UPDATE mats_data SET other_names = $othernames WHERE id = $index");
+		}
 
 
+		function tongyici_wenzhi($word_ori){
+			$config = array('SecretId'        => 'AKID7D4e6ZO4QAnbsluShmfmf09GoJl3YjlG',
+			             'SecretKey'       => 'RdkT3XGmusbDSQNLs8JNHxVqyUwI59xV',
+			             'RequestMethod'  => 'POST',
+			             'DefaultRegion'    => 'gz');
 
+			$wenzhi = QcloudApi::load(QcloudApi::MODULE_WENZHI, $config);
 
-		// require_once 'qcloudapi-sdk-php-master/src/QcloudApi/QcloudApi.php';
-		// $config = array('SecretId'        => 'AKID7D4e6ZO4QAnbsluShmfmf09GoJl3YjlG',
-		//              'SecretKey'       => 'RdkT3XGmusbDSQNLs8JNHxVqyUwI59xV',
-		//              'RequestMethod'  => 'POST',
-		//              'DefaultRegion'    => 'gz');
+			$package = array("text"=>$word_ori);
 
-		// $wenzhi = QcloudApi::load(QcloudApi::MODULE_WENZHI, $config);
+			$a = $wenzhi->LexicalSynonym($package);
 
-		// $package = array("text"=>"好像");
-
-		// $a = $wenzhi->LexicalSynonym($package);
-
-		// if ($a === false) {
-		//     $error = $wenzhi->getError();
-		//     echo "Error code:" . $error->getCode() . ".\n";
-		//     echo "message:" . $error->getMessage() . ".\n";
-		//     echo "ext:" . var_export($error->getExt(), true) . ".\n";
-		// } else {
-		//     // print ($a["syns"][0]["word_syns"][0]["text"]);
-		//     $Synonyms = $a["syns"][0]["word_syns"];
-		//     foreach ($Synonyms as $value) {
-		//     	$Synonym = $value["text"];//所有同义词个体
-		//     	// print($Synonym);
-		//     }
-		// }
+			if ($a === false) {
+			    // $error = $wenzhi->getError();
+			    // echo "Error code:" . $error->getCode() . ".\n";
+			    // echo "message:" . $error->getMessage() . ".\n";
+			    // echo "ext:" . var_export($error->getExt(), true) . ".\n";
+			    return nulll;
+			} else {
+			    // print ($a["syns"][0]["word_syns"][0]["text"]);
+			    $Synonyms = $a["syns"][0]["word_syns"];
+			    if($Synonyms){
+			    	$content = array();
+			    	foreach ($Synonyms as $value) {
+				    	$Synonym = $value["text"];//所有同义词个体
+				    	array_push($content, $Synonym);
+				    	// print($Synonym . " ");
+				    }
+				    return $content;
+			    }else{
+			    	// echo "无同义词";
+			    	return null;
+			    }
+			    // print_r($content);
+			}
+		}
+		
 
 
 		?>
