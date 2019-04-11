ab12527723e81ea54c1a8b75e948f4a98b96cea0
Zhaozhou Guo <yidanguo1102@gmail.com>
2019-04-11 15:52:49 +0800

    前端设计完成

--- _js/home.js
+++ _js/home.js
@@ -1,10 +1,71 @@
+// if (window.devicePixelRatio == 2) {
+// 	var metaList = document.getElementsByTagName("meta");
+// 	for (var i = 0; i < metaList.length; i++) {
+// 	  if (metaList[i].getAttribute("name") == "viewport") {
+// 	    metaList[i].setAttribute("content","width=device-width, initial-scale=0.5, maximum-scale=0.5, minimum-scale=0.5, user-scalable=no");
+// 	  }
+// 	}
+// }
+
+// if (window.devicePixelRatio == 3) {
+// 	var metaList = document.getElementsByTagName("meta");
+// 	for (var i = 0; i < metaList.length; i++) {
+// 	  if (metaList[i].getAttribute("name") == "viewport") {
+// 	    metaList[i].setAttribute("content","width=device-width, initial-scale=0.3333333333333333, maximum-scale=0.3333333333333333, minimum-scale=0.3333333333333333, user-scalable=no");
+// 	  }
+// 	}
+// }
+
 $(document).ready(function(){
 	// console.log('hello');
 	$('#searchSection').find('input')[0].addEventListener("focus", inputFocusStyle);
 	$('#searchSection').find('input')[0].addEventListener("blur", inputBlurStyle);
 	searchTitleLoop();
+	cursorLoop();
+	$('div.showmore p').click(hiddingtbsIn);
+	$('div.showless img').click(hiddingtbsOut);
 });
 
+
+function cursorLoop(){
+	setInterval(cLoop,2500);
+	function cLoop(){
+		$('#myCursor').animate({opacity:'0'},1000,function(){
+			// console.log('myCursor loop');
+			$('#myCursor').animate({opacity:'1'},500);
+		});
+	}
+	
+}
+
+function hiddingtbsIn(){
+	// console.log($(this).parents('.foodSection'));
+	var parentSection = $(this).parents('.foodSection');
+	parentSection.children('div.contentLeft').stop().animate({width:'0%'},100);
+	parentSection.children('div.contentRight').stop().animate({width:'100%'},100,function(){
+		parentSection.children('div.showmore').stop().animate({height:'0',opacity:'0'},200);
+		parentSection.children('div.showless').stop().animate({height:'50px',opacity:'1',marginTop:'30px'},200);
+		parentSection.children('div.hiddingtbs').show();
+		parentSection.children('div.hiddingtbs').stop().animate({opacity:'1'},200);
+	});
+}
+
+function hiddingtbsOut(){
+	var parentSection = $(this).parents('.foodSection');
+	var cScroll = $(window).scrollTop();
+	var height1 = parentSection.height();
+	// var secBotToWin = parentSection[0].getBoundingClientRect();
+	// secBotToWin = secBotToWin.top + height1;
+	parentSection.children('div.hiddingtbs').stop().animate({opacity:'0'},0);
+	parentSection.children('div.hiddingtbs').hide();
+	var height2 = parentSection.height();
+	var posTop = cScroll - (height1 - height2);
+	window.scrollTo(0,posTop);	
+	parentSection.children('div.showmore').stop().animate({height:'50px',opacity:'1'},300);
+	parentSection.children('div.showless').stop().animate({height:'0',opacity:'0',marginTop:'0px'},300);
+	parentSection.children('div.contentLeft').stop().animate({width:'23%'},200);
+	parentSection.children('div.contentRight').stop().animate({width:'77%'},200);
+}
 
 function searchBoxIn(){
 	$('.leftBox').css({'width':'15%','transition':'width 0.5s'});
@@ -23,6 +84,10 @@ function searchTitleLoop(){
 	setInterval(loop,5000);
 	function loop(){
 		// $('#titleWrapper').css({'margin-top':'-='+moveDis+'px'});
+		// console.log($('.titleContent')[1]);
+		var nextContent = $('.titleContent')[1];
+		var nextColor = nextContent.getAttribute("bgc");
+		$("#myCursor").css({'background-color': nextColor});
 		$("#titleWrapper").animate({marginTop:"-"+moveDis+"px"},500,function(){
 			$('.titleContent').first().appendTo($('#titleWrapper'));
 			$('#titleWrapper').css({'margin-top':'0px'});
@@ -65,6 +130,7 @@ function inputFocusStyle(){
 	searchLowerUp();
 	$('#content').hide();
 	$('#floater').hide();
+	$('#myCursor').hide();
 }
 
 function inputBlurStyle(){
@@ -74,5 +140,6 @@ function inputBlurStyle(){
 		searchLowerDown();
 		$('#content').show();
 		$('#floater').show();
+		$('#myCursor').show();
 	}
 }
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 	<head> 
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
@@ -27,14 +28,15 @@
 				<div id="searchBox">
 					<div id="searchTitle" class="leftBox">
 						<div id="titleWrapper">
-							<div class="titleContent"><img src="_img/icons/Vegetables-12.svg" alt=""></div>
-							<div class="titleContent"><img src="_img/icons/Vegetables-07.svg" alt=""></div>
-							<div class="titleContent"><img src="_img/icons/Vegetables-04.svg" alt=""></div>
-							<div class="titleContent"><img src="_img/icons/Vegetables-22.svg" alt=""></div>
+							<div class="titleContent" bgc="#ffd110"><img src="_img/icons/Vegetables-12.svg" alt=""></div>
+							<div class="titleContent" bgc="#52ddb2"><img src="_img/icons/Vegetables-07.svg" alt=""></div>
+							<div class="titleContent" bgc="#ffd110"><img src="_img/icons/Vegetables-04.svg" alt=""></div>
+							<div class="titleContent" bgc="#ff5576"><img src="_img/icons/Vegetables-22.svg" alt=""></div>
 						</div>
 					</div>
 					<form action="" class="medBox">
-						<input id="search_ingred" class="text_regular" type="search" value="" placeholder="搜索食材、蔬果等信息" />
+						<div id="myCursor"></div>
+						<input id="search_ingred" class="text_regular" type="search" value="" placeholder="搜索食材、蔬果信息" />
 					</form>
 					<div class="rightBox text_regular">
 						<p>取消</p>
@@ -63,28 +65,418 @@
 					</div>
 				</div>
 			</div><!-- END OF searchSection -->
-
-<!-- =================CONTENT================== -->
+		<!-- =================CONTENT================== -->
 			<div id="content">
-				<!-- <h1>content</h1>
-				<div id="testLen"></div> -->
 				<div class="foodSection">
 					<div class="contentLeft">
 						<div class="contentLeftWrapper">
 							<div class="leftImgWrapper">
-								<img src="_img/foodpics/40c829216cae4d189d4f89b1a16f0663.jpg" alt="">
+								<img src="_img/foodpics/e5c274bfa8c7474db603edea8a3d205e.jpg" alt="">
 							</div>
 						</div>
 					</div>
 					<div class="contentRight">
 						<div class="rightInner">
 							<p class="text_small title_small">食材</p>
-							<h1 class="fontZihun">红枣</h1>
+							<h1 class="fontZihun">桃</h1>
+							<!-- <h2 class="text_small">基本营养</h2> -->
+							<table class="basicInfo" border="0" cellpadding="0">
+								<tr class="text_small">
+									<th class="col_1">营养（致病）元素</th>
+									<th class="col_2">每100g</th>
+									<th class="col_3">备注</th>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">能量</td>
+									<td class="col_2">51<span class="units">千卡</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">蛋白质</td>
+									<td class="col_2">0.9<span class="units">克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">脂肪</td>
+									<td class="col_2">0.1<span class="units">克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">碳水化合物</td>
+									<td class="col_2">12.2<span class="units">克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">粗纤维</td>
+									<td class="col_2">1.3<span class="units">克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">嘌呤</td>
+									<td class="col_2">-</td>
+									<td class="col_3"><span class="red">高</span></td>
+								</tr>
+							</table>
+						</div><!-- END OF rightInner -->
+					</div><!-- END OF contentRight -->
+					<div class="showmore">
+						<p class="text_regular">更多营养指数</p>
+					</div>
+					<div class="hiddingtbs text_regular" >
+						<div class="rightInner">
+							<h2 class="text_small">脂类</h2>
+							<table class="basicInfo" border="0" cellpadding="0">
+								<tr class="text_small">
+									<th class="col_1">营养（致病）元素</th>
+									<th class="col_2">每100g</th>
+									<th class="col_3">备注</th>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">单不饱和脂肪酸</td>
+									<td class="col_2">0.067<span class="units">千卡</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">多不饱和脂肪酸</td>
+									<td class="col_2">0.086<span class="units">克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">多不饱和脂肪酸占总比</td>
+									<td class="col_2">50<span class="units">%</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">反式脂肪酸</td>
+									<td class="col_2">-<span class="units"></span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">反式脂肪酸占总比</td>
+									<td class="col_2">-<span class="units"></span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">胆固醇</td>
+									<td class="col_2">-</td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">植物固醇</td>
+									<td class="col_2">-</td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">胡萝卜素</td>
+									<td class="col_2">162<span class="units">微克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">叶黄素类</td>
+									<td class="col_2">158<span class="units">微克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">番茄红素</td>
+									<td class="col_2">-<span class="units"></span></td>
+									<td class="col_3"></td>
+								</tr>
+							</table>
+
+							<h2 class="text_small">矿物质</h2>
+							<table class="basicInfo" border="0" cellpadding="0">
+								<tr class="text_small">
+									<th class="col_1">营养（致病）元素</th>
+									<th class="col_2">每100g</th>
+									<th class="col_3">备注</th>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">钙</td>
+									<td class="col_2">6<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">镁</td>
+									<td class="col_2">7<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">钠</td>
+									<td class="col_2">5.7<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">钾</td>
+									<td class="col_2">166<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">磷</td>
+									<td class="col_2">20<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">硫</td>
+									<td class="col_2">5.84<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">氯</td>
+									<td class="col_2">8.79<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">铁</td>
+									<td class="col_2">0.8<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">碘</td>
+									<td class="col_2">-<span class="units"></span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">锌</td>
+									<td class="col_2">0.34<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">硒</td>
+									<td class="col_2">0.24<span class="units">微克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">铜</td>
+									<td class="col_2">0.05<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">锰</td>
+									<td class="col_2">0.07<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">氟</td>
+									<td class="col_2">4<span class="units">微克</span></td>
+									<td class="col_3"></td>
+								</tr>
+							</table>
 						</div>
 					</div>
+					<div class="showless">
+						<img src="_img/icons/doubleAr_up.svg" alt="">
+						<div class="borderBot"></div>
+						<!-- <p class="text_regular">隐藏基本营养</p> -->
+					</div>
+					<div class="clear"></div>
 				</div>
-			</div>
-		</div>
+
+				<div class="foodSection">
+					<div class="contentLeft">
+						<div class="contentLeftWrapper">
+							<div class="leftImgWrapper">
+								<img src="_img/foodpics/12c4e81ab4c44b729e1e9e1196a4b4f1.jpg" alt="">
+							</div>
+						</div>
+					</div>
+					<div class="contentRight">
+						<div class="rightInner">
+							<p class="text_small title_small">食材</p>
+							<h1 class="fontZihun">三文鱼</h1>
+							<!-- <h2 class="text_small">基本营养</h2> -->
+							<table class="basicInfo" border="0" cellpadding="0">
+								<tr class="text_small">
+									<th class="col_1">营养（致病）元素</th>
+									<th class="col_2">每100g</th>
+									<th class="col_3">备注</th>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">能量</td>
+									<td class="col_2">51<span class="units">千卡</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">蛋白质</td>
+									<td class="col_2">0.9<span class="units">克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">脂肪</td>
+									<td class="col_2">0.1<span class="units">克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">碳水化合物</td>
+									<td class="col_2">12.2<span class="units">克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">粗纤维</td>
+									<td class="col_2">1.3<span class="units">克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">嘌呤</td>
+									<td class="col_2">-</td>
+									<td class="col_3"><span class="red">高</span></td>
+								</tr>
+							</table>
+						</div><!-- END OF rightInner -->
+					</div><!-- END OF contentRight -->
+					<div class="showmore">
+						<p class="text_regular">更多营养指数</p>
+					</div>
+					<div class="hiddingtbs text_regular" >
+						<div class="rightInner">
+							<h2 class="text_small">脂类</h2>
+							<table class="basicInfo" border="0" cellpadding="0">
+								<tr class="text_small">
+									<th class="col_1">营养（致病）元素</th>
+									<th class="col_2">每100g</th>
+									<th class="col_3">备注</th>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">单不饱和脂肪酸</td>
+									<td class="col_2">0.067<span class="units">千卡</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">多不饱和脂肪酸</td>
+									<td class="col_2">0.086<span class="units">克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">多不饱和脂肪酸占总比</td>
+									<td class="col_2">50<span class="units">%</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">反式脂肪酸</td>
+									<td class="col_2">-<span class="units"></span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">反式脂肪酸占总比</td>
+									<td class="col_2">-<span class="units"></span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">胆固醇</td>
+									<td class="col_2">-</td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">植物固醇</td>
+									<td class="col_2">-</td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">胡萝卜素</td>
+									<td class="col_2">162<span class="units">微克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">叶黄素类</td>
+									<td class="col_2">158<span class="units">微克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">番茄红素</td>
+									<td class="col_2">-<span class="units"></span></td>
+									<td class="col_3"></td>
+								</tr>
+							</table>
+
+							<h2 class="text_small">矿物质</h2>
+							<table class="basicInfo" border="0" cellpadding="0">
+								<tr class="text_small">
+									<th class="col_1">营养（致病）元素</th>
+									<th class="col_2">每100g</th>
+									<th class="col_3">备注</th>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">钙</td>
+									<td class="col_2">6<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">镁</td>
+									<td class="col_2">7<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">钠</td>
+									<td class="col_2">5.7<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">钾</td>
+									<td class="col_2">166<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">磷</td>
+									<td class="col_2">20<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">硫</td>
+									<td class="col_2">5.84<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">氯</td>
+									<td class="col_2">8.79<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">铁</td>
+									<td class="col_2">0.8<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">碘</td>
+									<td class="col_2">-<span class="units"></span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">锌</td>
+									<td class="col_2">0.34<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">硒</td>
+									<td class="col_2">0.24<span class="units">微克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">铜</td>
+									<td class="col_2">0.05<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">锰</td>
+									<td class="col_2">0.07<span class="units">毫克</span></td>
+									<td class="col_3"></td>
+								</tr>
+								<tr class="text_regular">
+									<td class="col_1">氟</td>
+									<td class="col_2">4<span class="units">微克</span></td>
+									<td class="col_3"></td>
+								</tr>
+							</table>
+						</div>
+					</div>
+					<div class="showless">
+						<img src="_img/icons/doubleAr_up.svg" alt="">
+						<div class="borderBot"></div>
+						<!-- <p class="text_regular">隐藏基本营养</p> -->
+					</div>
+					<div class="clear"></div>
+				</div>
+				<div class="footer"></div>
+			</div><!-- END OF CONTENT -->
+		</div><!-- END OF WRAPPER -->
 	</body>
 </html>
 
